9bd45dd3db527af1d17247c94570c7a8af5e2834
Signeal <139138171+Signeal@users.noreply.github.com>
2023-07-15 14:18:37 -0400

    final tech demo i promise

--- src/main.py
+++ src/main.py
@@ -7,12 +7,17 @@ import Input
 FPS: int = 60
 
 pg.init()
-screen = pg.display.set_mode((300, 300), pg.RESIZABLE)  # setup
+screen = pg.display.set_mode((1200, 675), pg.RESIZABLE)  # setup, 16:9 screen ratio
 pg.display.set_caption("Epic Game 🐸")
 pg.display.set_icon(assets.icon)
 
-hue = 0
+greet = pg.font.Font("freesansbold.ttf", 64).render("HELLO", True, (0, 0, 0))
+greetRect = greet.get_rect()
 
+text = pg.font.Font("freesansbold.ttf", 64).render("I'M GOING TO RGBEAT YOU", True, (0, 0, 0))
+textRect = text.get_rect()
+
+hue = 0
 running = True
 while running:  # start game loop
     startMillis: int = pg.time.get_ticks()  # time at start
@@ -27,6 +32,12 @@ while running:  # start game loop
     screen.blit(assets.testImg,
                 ((screen.get_size()[0] / 2) - (assets.testImg.get_size()[0] / 2),  # display phrog in middle
                  (screen.get_size()[1] / 2) - (assets.testImg.get_size()[1] / 2)))
+
+    greetRect.center = (screen.get_size()[0] / 2, (greetRect.size[1] / 2) + (screen.get_size()[1] / 7))
+    screen.blit(greet, greetRect)
+
+    textRect.center = (screen.get_size()[0] / 2, screen.get_size()[1] - (textRect.size[1] / 2) - (screen.get_size()[1] / 7))
+    screen.blit(text, textRect)
 
     hue += 1  # important
 
