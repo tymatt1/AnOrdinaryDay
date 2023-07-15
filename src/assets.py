c9dda76583defd5d95d35e7d894cc4d2062f6c80
Signeal <139138171+Signeal@users.noreply.github.com>
2023-07-14 21:48:22 -0400

    tech demo

---
+++ src/assets.py
@@ -0,0 +1,4 @@
+import pygame as pg
+
+icon = pg.image.load("assets/icon.png")
+testImg = pg.image.load("assets/phrog.png")
--- src/main.py
+++ src/main.py
@@ -1,19 +1,36 @@
 import pygame as pg
-import StoryUI as s
+import assets
+# import StoryUI as s
 import Input
 
 
-screen = pg.display.set_mode((300, 300), pg.RESIZABLE)
+FPS: int = 60
+
+pg.init()
+screen = pg.display.set_mode((300, 300), pg.RESIZABLE)  # setup
 pg.display.set_caption("Epic Game 🐸")
+pg.display.set_icon(assets.icon)
+
+hue = 0
 
 running = True
-while running:     # start game loop
+while running:  # start game loop
+    startMillis: int = pg.time.get_ticks()  # time at start
+
     Input.handle()
     if Input.stop: running = False
 
-    test = s.TextBox("hi")
+    c = pg.Color(0, 0, 0, 0)
+    c.hsva = (hue % 360, 100, 100, 100)  # background color
+    screen.fill(c)
 
-    screen.fill((255, 0, 0))
-    pg.display.flip()
+    screen.blit(assets.testImg,
+                ((screen.get_size()[0] / 2) - (assets.testImg.get_size()[0] / 2),  # display phrog in middle
+                 (screen.get_size()[1] / 2) - (assets.testImg.get_size()[1] / 2)))
+
+    hue += 1  # important
+
+    pg.display.flip()  # update display
+    pg.time.wait(int(1000 / FPS) - (pg.time.get_ticks() - startMillis))  # do math to fps limit the game
 
 pg.quit()
