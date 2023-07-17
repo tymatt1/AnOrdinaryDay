83111fea7f493b134ace286028c9f408d83d7d40
Signeal <139138171+Signeal@users.noreply.github.com>
2023-07-17 15:00:44 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,13 +8,6 @@ import renderHelper as rh
 FPS = 60
 
 rh.init("Epic Game 🐸", Assets.icon)
-
-greet = pg.font.Font("freesansbold.ttf", 64).render("HELLO", True, (0, 0, 0))
-greetRect = greet.get_rect()
-
-text = pg.font.Font("freesansbold.ttf", 64).render("I'M GOING TO RGBEAT YOU", True, (0, 0, 0))
-textRect = text.get_rect()
-
 scenes.start.start()
 
 hue = 0
@@ -28,16 +21,6 @@ while running:  # start game loop
     c = pg.Color(0, 0, 0, 0)
     c.hsva = (hue % 360, 100, 100, 100)  # background color
     rh.screen.fill(c)
-
-    rh.screen.blit(Assets.testImg,
-                ((rh.screen.get_size()[0] / 2) - (Assets.testImg.get_size()[0] / 2),  # display phrog in middle
-                (rh.screen.get_size()[1] / 2) - (Assets.testImg.get_size()[1] / 2)))
-
-    greetRect.center = (rh.screen.get_size()[0] / 2, (greetRect.size[1] / 2) + (rh.screen.get_size()[1] / 7))
-    rh.screen.blit(greet, greetRect)
-
-    textRect.center = (rh.screen.get_size()[0] / 2, rh.screen.get_size()[1] - (textRect.size[1] / 2) - (rh.screen.get_size()[1] / 7))
-    # rh.screen.blit(text, textRect)
 
     hue += 1  # important
 
