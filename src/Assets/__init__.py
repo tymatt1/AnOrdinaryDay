15d84c6a280dcc41ce4deb9c840ba63b9615cc39
Signeal <139138171+Signeal@users.noreply.github.com>
2023-07-17 15:09:59 -0400

    add new images

--- src/Assets/__init__.py
+++ src/Assets/__init__.py
@@ -4,7 +4,12 @@ import os
 
 path = os.path.dirname(os.path.abspath(__file__))  # need to add the path or it won't run when double clicked
 
+# misc
 icon = pg.image.load(path + "/icon.png")
-diningHall = pg.image.load(path + "/diningHall/dininghall.png")
 testImg = pg.image.load(path + "/phrog.png")
 character = pg.image.load(path + "/characterSprite/charStill.png")
+
+# backgrounds
+diningHall = pg.image.load(path + "/backgrounds/diningHall/dininghall.png")
+bedroom = pg.image.load(path + "/backgrounds/bedroom.png")
+sixFlags = pg.image.load(path + "/backgrounds/Six Flags.png")
