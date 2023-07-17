1b9bba5e61668c128f6124de625c5f891b2095ff
Signeal <139138171+Signeal@users.noreply.github.com>
2023-07-17 11:42:30 -0400

    idk

--- src/renderHelper.py
+++ src/renderHelper.py
@@ -21,6 +21,20 @@ def init(text: string, icon: pg.Surface):
     pg.display.set_icon(icon)
 
 
+<<<<<<< Updated upstream
+def drawImg(img: pg.Surface, pos: tuple, dims: tuple):
+    img = pg.transform.scale(img, (
+            width() if dims[0] == -1 else dims[0],
+            height() if dims[1] == -1 else dims[1]
+        ))
+
+    screen.blit(img, (
+            (width() / 2) - (img.get_size()[0] / 2) if pos[0] == -1 else pos[0],
+            (height() / 2) - (img.get_size()[1] / 2) if pos[1] == -1 else pos[1]
+        ))
+
+=======
+>>>>>>> Stashed changes
 def drawRect(color: tuple, pos: tuple, dims: tuple):
     s = pg.Surface(dims)  # size
     s.set_alpha(color[3])
@@ -29,6 +43,7 @@ def drawRect(color: tuple, pos: tuple, dims: tuple):
     screen.blit(s, pos)
 
 def drawText(text: string, fontSize: int, pos: tuple, color: tuple = (255, 255, 255), font: string = "freesansbold.ttf"):
+<<<<<<< Updated upstream
     if "\n" in text:
         lines: tuple = text.split("\n")
         middle = (len(lines) - 1) / 2
@@ -36,6 +51,8 @@ def drawText(text: string, fontSize: int, pos: tuple, color: tuple = (255, 255, 
             drawText(lines[i], fontSize, (pos[0], pos[1] + ((i - middle) * fontSize)), color, font)
         return
 
+=======
+>>>>>>> Stashed changes
     text = pg.font.Font(font, fontSize).render(text, True, color)
     textRect = text.get_rect()
     textRect.centerx = width() / 2 if pos[0] == -1 else pos[0]
--- src/scenes.py
+++ src/scenes.py
@@ -3,4 +3,14 @@ import Assets
 
 
 currentScene = None
-start = Scene(None, Assets.testImg, TextBox("this is a long string of text that i am writing\nfor demonstration purposes woiaiweoi fhaef uhaijs fbais"))  # make first scene current so it starts
+<<<<<<< Updated upstream
+
+start = Scene(None, Assets.testImg,
+        # TextBox("this is a long string of text that i am writing\nfor demonstration purposes wow"),
+        Decision(
+                ("commit a crime", Scene(None, Assets.testImg, TextBox("jail")))
+            )
+    )
+=======
+start = Scene(None, Assets.testImg, TextBox("You wake up in a laaaaaaaaaaaaaaaaaaaaaaaateyrsetdrydtesrtdfkyutrd\nfcgjhvtufyaaaaaaaaaarge room"))  # make first scene current so it starts
+>>>>>>> Stashed changes
