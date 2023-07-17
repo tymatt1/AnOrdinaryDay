d3da42bcd931894deaf56c57e702be4ae9b9af6c
Signeal <139138171+Signeal@users.noreply.github.com>
2023-07-17 14:18:21 -0400

    image stuff

--- src/Assets/__init__.py
+++ src/Assets/__init__.py
@@ -5,5 +5,6 @@ import os
 path = os.path.dirname(os.path.abspath(__file__))  # need to add the path or it won't run when double clicked
 
 icon = pg.image.load(path + "/icon.png")
+diningHall = pg.image.load(path + "/diningHall/dininghall.png")
 testImg = pg.image.load(path + "/phrog.png")
 character = pg.image.load(path + "/characterSprite/charStill.png")
--- src/scenes.py
+++ src/scenes.py
@@ -4,12 +4,10 @@ import Assets
 
 currentScene = None
 
-start = Scene(None, Assets.testImg,
-        TextBox("this is a long string of text that i am writing\nfor demonstration purposes wow"),
+start = Scene(None, Assets.diningHall,
+        TextBox("wow theres food here aaaaaa"),
         Decision(
                 ("commit a crime", Scene(None, Assets.testImg, TextBox("jail"))),
-                ("consume a bagel", Scene(Scene(None, Assets.icon, TextBox("execution")), Assets.testImg, TextBox("jail"))),
-                ("consume a bagel", Scene(Scene(None, Assets.icon, TextBox("execution")), Assets.testImg, TextBox("jail"))),
-                ("consume a bagel", Scene(Scene(None, Assets.icon, TextBox("execution")), Assets.testImg, TextBox("jail"))),
+                ("consume a bagel", Scene(Scene(None, Assets.icon, TextBox("execution")), Assets.testImg, TextBox("jail")))
             )
     )
