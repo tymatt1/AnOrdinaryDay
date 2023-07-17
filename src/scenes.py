07777bdf3d3332d60fc3c7675f6f91cc274fbb49
Signeal <139138171+Signeal@users.noreply.github.com>
2023-07-17 11:49:11 -0400

    bagel

--- src/scenes.py
+++ src/scenes.py
@@ -7,6 +7,7 @@ currentScene = None
 start = Scene(None, Assets.testImg,
         # TextBox("this is a long string of text that i am writing\nfor demonstration purposes wow"),
         Decision(
-                ("commit a crime", Scene(None, Assets.testImg, TextBox("jail")))
+                ("commit a crime", Scene(None, Assets.testImg, TextBox("jail"))),
+                ("consume a bagel", Scene(None, Assets.testImg, TextBox("jail")))
             )
     )
