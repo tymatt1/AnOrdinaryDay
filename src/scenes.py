808dbf8d8af421e5beaa452e9d21862c27a429bc
Signeal <139138171+Signeal@users.noreply.github.com>
2023-07-20 09:11:46 -0400

    minor scene changes

--- src/scenes.py
+++ src/scenes.py
@@ -5,31 +5,31 @@ import Assets
 badStomach = Scene(None, Assets.diningHall, StaticsList(),
               TextBox("Your stomach doesn't feel so good..."),
               Decision(
-                  ("Go to the bathroom", (), Scene(walkToClass, Assets.diningHall, StaticsList(), TextBox("Much better, time to go to class"))),
-                  ("Go to class", (), Scene(None, Assets.poopDeath, StaticsList(), TextBox("You Pooped your pants"))),
+                  ("Go to the bathroom", (), Scene(walkToClass, Assets.diningHall, StaticsList(), TextBox("Much better. Time to go to class."))),
+                  ("Go to class", (), Scene(None, Assets.poopDeath, StaticsList(), TextBox("You pooped your pants.\nThe world fades as you fall to the ground."))),
               ))
 
 
 dine = Scene(None, Assets.diningHall, StaticsList(),
-              TextBox("What do you want to eat"),
+              TextBox("What do you want to eat?"),
               Decision(
-                  ("Cereal", ("jerry", "true"), Scene(walkToClass, Assets.road, StaticsList(), TextBox("Time to go to class"))),
-                  ("Pancakes", ("matthew", "true"), Scene(walkToClass, Assets.road, StaticsList(), TextBox("Time to go to class"))),
-                  ("Oatmeal", ("duy", "true"), Scene(walkToClass, Assets.road, StaticsList(), TextBox("Time to go to class"))),
+                  ("Cereal", ("jerry", "true"), Scene(walkToClass, Assets.road, StaticsList(), TextBox("Time to go to class.\nJerry follows you because of your cerealicious scent."))),
+                  ("Pancakes", ("matthew", "true"), Scene(walkToClass, Assets.road, StaticsList(), TextBox("Time to go to class.\nMatthew follows you because of your pancake-y scent"))),
+                  ("Oatmeal", ("duy", "true"), Scene(walkToClass, Assets.road, StaticsList(), TextBox("Time to go to class.\nDuy follows you because of your oatmealish scent."))),
                   ("Eggs and Sausage", (), Scene(badStomach, Assets.icon, StaticsList(), TextBox("Uh Oh"))),
               ))
 # new branch example
 
 wakeAt8 = Scene(dine, Assets.bedroom, StaticsList(),
-                TextBox("You wake up feeling energized"),
+                TextBox("You wake up feeling energized."),
                 TextBox("Oh my turtles!\nIt's 8:15 am!"),
                 Character(Assets.main.stillLeft, (100, 80), (900, 260), (100, 260), 1))
 
 
 wakeRoommate = Scene(None, Assets.bedroom, StaticsList(),
                      Decision(
-                         ('Wake Up Your Roommate', ("anime", "true"), Scene(dine, Assets.bedroom, StaticsList(), TextBox("You woke up your rooommate"))),
-                         ("Don't wake up your roommate", ("imaginary", "true"), Scene(dine, Assets.bedroom, StaticsList(), TextBox("You Roomate remains asleep"))),
+                         ("Wake up roommate", ("anime", "true"), Scene(dine, Assets.bedroom, StaticsList(), TextBox("You woke up Anime, your rooommate.\nHe follows you.\nYou go to breakfast."))),
+                         ("Don't wake up roommate", ("imaginary", "true"), Scene(dine, Assets.bedroom, StaticsList(), TextBox("Your roomate remains asleep.\nYou go to breakfast."))),
                      ))
 
 brushTeeth = Scene(None, Assets.bathroom, StaticsList(),
@@ -46,11 +46,11 @@ wakeAt9 = Scene(walkToClass, Assets.bedroom, StaticsList(),
 
 
 dorm = Scene(None, Assets.bedroom, StaticsList(),
-             TextBox("You wake up to your alarm.\nIt is 7:30 am."),
+             TextBox("You wake to your alarm.\nIt is 7:30 am."),
              Decision(
-                 ("Get ready", ("energy", "none"), Scene(brushTeeth, Assets.bedroom, StaticsList(), TextBox("Getting ready"), Character(Assets.main.stillLeft, (100, 80), (900, 260), (100, 260), 2.5)),),
-                 ("Go back to sleep", ("energy", "very"), Scene(wakeAt9, Assets.bedroom, StaticsList(), TextBox("Sleeping"))),
-                 ("Snooze alarm", ("energy", "some"), Scene(wakeAt8, Assets.bedroom, StaticsList(), TextBox("Sleeping")))
+                 ("Get ready", ("energy", "none"), Scene(brushTeeth, Assets.bedroom, StaticsList(), TextBox("Still tired, you wake up to start your day."), Character(Assets.main.stillLeft, (100, 80), (900, 260), (100, 260), 2.5)),),
+                 ("Go back to sleep", ("energy", "very"), Scene(wakeAt9, Assets.bedroom, StaticsList(), TextBox("You go back to sleep and gain massive amounts of evergy."))),
+                 ("Snooze alarm", ("energy", "some"), Scene(wakeAt8, Assets.bedroom, StaticsList(), TextBox("You go back to sleep and gain some energy.")))
              )
              )
 
