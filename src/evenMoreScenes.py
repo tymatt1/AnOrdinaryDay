d187d57ffbb95c50738b614d572e76bcffbc6836
Signeal <139138171+Signeal@users.noreply.github.com>
2023-07-20 10:41:37 -0400

    ending sounds

--- src/evenMoreScenes.py
+++ src/evenMoreScenes.py
@@ -3,9 +3,11 @@ import Assets
 
 
 coasterFall = Scene(None, Assets.coasterFall, StaticsList(),
+                    Sound(2),
                     TextBox("Somehow, your cart fell off at the highest point of the loop.\nYou go to the hospital with 206 broken bones."))
 
 winAtLife = Scene(None, Assets.funEnding, StaticsList(),
+                  Sound(1),
                   TextBox("The roller coaster broke down and killed everyone on it.\nThank god you didn't go on that. Anyways, you had fun at Six Flags\nand become a winner in life."))
 noCoaster = Scene(winAtLife, Assets.sixFlags, StaticsList(),
                   AttributeCheck(("anime", "true"), Scene(coasterFall, Assets.sixFlags, StaticsList(), TextBox("Anime forces you onto the roller coaster."))))
@@ -17,19 +19,20 @@ sixFlags = Scene(None, Assets.sixFlags, StaticsList(),
                      ("Don't go on roller coaster", (), noCoaster)
                  ))
 terrBreath = Scene(None, Assets.shot, StaticsList(),
-                  AttributeCheck(("breath", "good"), Scene(None, Assets.famous, StaticsList(), TextBox("You tropical smelling breath from your toothpaste charmed them \n making them turn themselves in, you are now internet famous"))),
-                  TextBox("Why didn't you try anything, you got shot"))
+                  AttributeCheck(("breath", "good"), Scene(None, Assets.famous, StaticsList(), Sound(1), TextBox("You tropical smelling breath from your toothpaste charmed them \n making them turn themselves in, you are now internet famous"))),
+                  Sound(2),
+                  TextBox("Why didn't you try anything, you got shot!"))
 
 fighter = Scene(terrBreath, Assets.shot, StaticsList(),
-                  AttributeCheck(("imaginary", "true"), Scene(None, Assets.savedFriend, StaticsList(), TextBox("They were scary, but your imaginary friend defeats them all"))),
-                  TextBox("It was 20v1 what were you thinking, you got shot"))
+                  AttributeCheck(("imaginary", "true"), Scene(None, Assets.savedFriend, StaticsList(), Sound(1), TextBox("They were scary, but your imaginary friend defeats them all"))),
+                  TextBox("It was 20 v 1. What were you thinking??? You got shot."))
 
 
 terroristsEnter = Scene(None, Assets.classroom, StaticsList(),
-              TextBox("A group of people terrorize your building, fight back?"),
+              TextBox("A group of people terrorize your building.\nDo you fight back?"),
               Decision(
-                  ("Yes, fight back", (), fighter),
-                  ("No don't do anything", (), terrBreath),
+                  ("Fight back", (), fighter),
+                  ("Don't do anything", (), terrBreath),
               ))
 
 
